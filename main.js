37129537fcc117c899b37ab125a4fae84dc950ef
CuauCabrera <cabrera.cuau@gmail.com>
2023-11-02 20:46:16 -0600

    Add computerChoice logic

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log('Hello Odin');
+/*SET COMPUTER'S TURN*/
+let computerTurn = Math.floor((Math.random() * 3) + 1);
+let computerChoice;  
+
+/*SET COMPUTER CHOICE*/
+if (computerTurn === 1) {
+    computerChoice = 'rock';
+    }else if (computerTurn === 2) {
+        computerChoice = 'paper';
+    }else if (computerTurn === 3) {
+        computerChoice = 'scissors';
+    }
+
+
